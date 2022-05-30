11f95b0683534748611a37bb361838865d290611
Des_Abd_Wis <destiniyabidingwisdom@gmail.com>
2022-05-30 11:17:28 +0300

    30.05.22

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -3,7 +3,9 @@ package utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import java.time.Duration;
 
 public class Driver {
@@ -14,24 +16,34 @@ public class Driver {
     ve en sonda driver'in kapatilmasi tercih edilmistir.
      */
     static WebDriver driver;
-
     public static WebDriver getDriver(){
-
         if (driver==null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            switch (ConfigReader.getProperty("browser")){
+                case "chrome" :
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "safari" :
+                    WebDriverManager.safaridriver().setup();
+                    driver=new SafariDriver();
+                    break;
+                case "firefox" :
+                    WebDriverManager.firefoxdriver().setup();
+                    driver=new FirefoxDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         }
         return driver;
     }
-
     public static void closeDriver(){
         if (driver!=null){ // driver'a deger atanmissa
             driver.close();
-           driver=null;
+            driver=null;
         }
-
     }
-
 }
